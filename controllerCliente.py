99a0b123877e7bc9bc96678b5214787bc0ef361e
Dahiam Castano <dahiamps@gmail.com>
2021-09-17 10:12:55 -0500

    Se crea metodo para crear el cliente

--- controllerCliente.py
+++ controllerCliente.py
@@ -1,5 +1,14 @@
 from dbconnection import getConnection
 
+
+def insert_client(ccnit,nombre,telefono,direccion,estado):
+    cnx = getConnection()
+    with cnx.cursor() as cursor:
+        cursor.execute("INSERT INTO cliente (ccnit,nombre,telefono,direccion,estado) VALUES (%s,%s,%s,%s,%s)",(ccnit,nombre,telefono,direccion,estado))
+    cnx.commit()
+    cnx.close()
+
+
 def get_clientes():
     cnx = getConnection()
     Clientes = []
--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import controllerCliente
 app = Flask(__name__)
 
 
+
+@app.route('/insert_cliente')
+def form_insert_material():
+    return render_template('insertcliente.html')
+
+@app.route('/save_client',methods=["POST"])
+def save_client():
+    ccnit = request.form["ccnit"]
+    nombre = request.form["nombre"]
+    telefono = request.form["telefono"]
+    direccion = request.form["direccion"]
+    estado = request.form["estado"]
+    controllerCliente.insert_client(ccnit, nombre, telefono, direccion, estado)
+    return redirect("/Clientes")
+
+
 @app.route("/")
 @app.route("/Clientes")
 def clientes():
